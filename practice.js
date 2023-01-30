b2a0e95a8d58f4023c83a371ef1422bb57cc1b94
MohonSaha <practicewithmohon@gmail.com>
2023-01-31 00:29:53 +0600

    add more problem

--- practice.js
+++ practice.js
@@ -1,12 +1,12 @@
 
 // Problem-1: Convert inch to feet 
 
-function inchToFeet(inches){
-    const feet = inches / 12;
-    return feet;
-}
+// function inchToFeet(inches){
+//     const feet = inches / 12;
+//     return feet;
+// }
 
-const result = inchToFeet(48).toFixed(2);
+// const result = inchToFeet(48).toFixed(2);
 // console.log(parseFloat(result));
 
 
@@ -50,35 +50,64 @@ const result = inchToFeet(48).toFixed(2);
 
 // Problem-4 (Method-2): 
 
-function getSum(numbers){
-    const oddNumbers = [];
-for(num in numbers){
-    let arrayNum = numbers[num];
-    const odd = arrayNum % 2;   
-    if(odd !== 0){      
-        oddNumbers.push(arrayNum);
-    }
-}
-return oddNumbers;
-}
+// function getSum(numbers){
+//     const oddNumbers = [];
+// for(num in numbers){
+//     let arrayNum = numbers[num];
+//     const odd = arrayNum % 2;   
+//     if(odd !== 0){      
+//         oddNumbers.push(arrayNum);
+//     }
+// }
+// return oddNumbers;
+// }
 
 
-function oddNumberSum(OddNumbers){
-    let sum = 0;
- for(let i =0; i<oddNumbers.length; i++){
-    let arrayNum = OddNumbers[i];
-    sum += arrayNum;
- }
- return sum;
-}
+// function oddNumberSum(OddNumbers){
+//     let sum = 0;
+//  for(let i =0; i<oddNumbers.length; i++){
+//     let arrayNum = OddNumbers[i];
+//     sum += arrayNum;
+//  }
+//  return sum;
+// }
+
+
+// const myNumber = [12, 1, 3, 78, 32, 5, 7];
+// const oddNumbers = getSum(myNumber);
+// const sumOddNumbers = oddNumberSum(oddNumbers);
+// console.log(sumOddNumbers);
 
 
 
-const myNumber = [12, 1, 3, 78, 32, 5, 7];
-const oddNumbers = getSum(myNumber);
-const sumOddNumbers = oddNumberSum(oddNumbers);
-console.log(sumOddNumbers);
+// Problem-5: Find the factorial of a number
 
+// function factorial(num){
+//     let fact = 1;
+//     for(let i = 1; i <= num; i++){
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// const result = factorial(4);
+// console.log(result);
+
+
+
+// Problem-5: Find the factorial of a number in reverse way
+
+// function factorial(num){
+//     let fact = 1;
+//     for(let i = num; i >= 1; i--){
+//         fact *= i;
+//         console.log(i);
+//     }
+//     return fact;
+// }
+
+// const result = factorial(4);
+// console.log(result);
 
 
 
